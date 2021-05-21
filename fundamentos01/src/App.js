15f4e6fe918f90d5f146d47747f2e75300906636
FernandaSacoli <fersacoli@gmail.com>
2021-05-21 12:58:14 +0000

    Commit- Aula 01

---
+++ fundamentos01/src/App.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react'
+
+import OlaMundo from './components/basico/olamundo'
+
+class App extends Component {
+  render () {
+    return(
+      <div>
+        <h1>Aula 01-Fundamentos</h1>
+        
+      <OlaMundo />
+      </div>
+
+    )
+  }
+
+}
+
+export default App;
