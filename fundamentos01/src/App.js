db9482d2bd5e0dccf621a66df9314013696e170f
FernandaSacoli <fersacoli@gmail.com>
2021-05-21 18:09:32 +0000

    Commit para salvar

--- fundamentos01/src/App.js
+++ fundamentos01/src/App.js
@@ -1,19 +1,31 @@
 import React, {Component} from 'react'
-
+import './App.css'
+import Card from './components/layout/Card'
+import Propriedades from './components/basico/Propriedades'
 import OlaMundo from './components/basico/OlaMundo'
 
 class App extends Component {
-  render () {
-    return(
-      <div>
-        <h1>Aula 01-Fundamentos</h1>
-        
-      <OlaMundo />
-      </div>
+	
+	render (){
+	return (
 
-    )
-  }
+	<div className="App" >
+		<h1>Exemploes React</h1>
+		<div className="Cards" >
 
+		<Card título="Propriedades">
+			<Propriedades nome="Homer" sobrenome="Jay Simpson" idade={38}/>
+			</Card>
+
+			<Card título="Olá Mundo!">
+			<OlaMundo />
+			</Card>
+
+			</div>
+			</div>
+	)
+
+	}
 }
 
 export default App;
